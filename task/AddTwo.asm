ed976faafc6708793e804927e0557d665eeed7b1
Abdul Mateen <98536971+notrealmateen@users.noreply.github.com>
2022-10-26 22:06:40 +0500

    Add files via upload

---
+++ task/AddTwo.asm
@@ -0,0 +1,65 @@
+INCLUDE Irvine32.inc
+.data
+	myArray1 BYTE 1,2,3,4,5
+	s = $-myarray1
+	l BYTE 0dh,0ah,0
+
+
+.code
+
+main PROC
+
+
+	call intSum
+	call ReadInt
+	mov ecx,eax
+
+	ptr1:
+		call Random32			
+		mov myArray1[0],al		
+		call intSum
+		loop ptr1
+	exit
+
+main ENDP	;end of main
+
+
+intSum Proc USES ecx
+	push ecx
+	mov ecx,s
+	mov esi , 0
+	;		loop 1
+	L1:
+		movzx eax,myarray1[esi]
+		push eax
+		inc esi
+		loop L1
+	mov ecx,s
+	mov esi , 0
+	;		loop 2
+	L2:
+		pop eax
+		mov myarray1[esi],al
+		inc esi
+		loop L2
+	mov ecx,s
+	mov esi , 0
+	mov eax,0
+	;		loop 3
+	L3:
+		add al,myarray1[esi]
+		inc esi
+		loop L3			
+	mov edx , offset l
+	call writeBin
+	call writeString
+	call writeHex
+	call writeString
+	call writeInt
+	call writeString
+	pop ecx
+	ret	
+
+intSum ENDP
+
+END main
